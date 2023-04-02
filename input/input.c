192eef8cc6094e69be200dce52dec34be5114620
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-04-02 21:11:44 +0700

    input

---
+++ input/input.c
@@ -0,0 +1,28 @@
+// input  = scanf()
+
+#include <stdio.h>
+int main (){
+  
+   int a,b;
+   scanf("%d %d",&a,&b);  // "%d %f ", &int &float
+   printf("%d %d",a,b);
+    return 0 ;
+}
+
+
+// & = its name address of in c programming  scanf
+
+
+
+
+// note
+
+
+// format specifier for printing
+// int  = %d
+// float = %f
+// char = %c
+// bool = 
+
+// if you see a specify float number then follow this
+//%0.(set number what you want)f   like %0.2f
