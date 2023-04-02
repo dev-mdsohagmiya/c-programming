2bc3999b9f8ea3b9d83b77906617920e67c08b92
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-04-02 19:58:59 +0700

    data-type-and-variable

---
+++ variable-and-data-types/vadt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+
+int sohag = 10;
+float fsohag = 10000.5;
+char csohag = 'A';
+
+
+printf("%d %0.4f %c",sohag,fsohag,csohag);
+return 0;
+}
+
+
+
+
+
+// format specifier for printing
+// int  = %d
+// float = %f
+// char = %c
+// bool = 
+
+// if you see a specify float number then follow this
+//%0.(set number what you want)f   like %0.2f
